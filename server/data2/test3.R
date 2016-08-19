45c20ab002f7cff444179fb07d73b9ce24ecd9f4
jay <xiangxz@cn.ibm.com>
2016-08-19 11:56:19 +0800

    x

--- server/data2/test3.R
+++ server/data2/test3.R
@@ -5,10 +5,10 @@ library(DT)
 process_data <- read.csv("process.csv", header = TRUE, sep = ",")
 datatable(process_data)
 
-# library(car)
-# ga_data <- read.csv("trans.csv", header = TRUE, sep = ",")
-# scatterplotMatrix(~ ga_data$centile + ga_data$rank + ga_data$income, 
-#                   data=ga_data, spread=FALSE,
-#                   lty.smooth=2, main="Relation Matrix")
+library(car)
+ga_data <- read.csv("trans.csv", header = TRUE, sep = ",")
+scatterplotMatrix(~ ga_data$centile + ga_data$rank + ga_data$income, 
+                  data=ga_data, spread=FALSE,
+                  lty.smooth=2, main="Relation Matrix")
                   
 Sys.sleep(1000)
