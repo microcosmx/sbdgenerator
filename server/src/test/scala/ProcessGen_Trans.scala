3d6879ce90f73e3bef1acd7da34695d3b02f0567
jay <xiangxz@cn.ibm.com>
2016-08-24 10:07:16 +0800

    xx

---
+++ server/src/test/scala/ProcessGen_Trans.scala
@@ -0,0 +1,171 @@
+import akka.actor._
+import akka.io.IO
+import akka.pattern._
+import akka.routing._
+import akka.util.Timeout
+import org.apache.hadoop.fs._
+import org.apache.avro._
+import org.apache.avro.file._
+import org.apache.avro.reflect._
+import org.apache.hadoop.fs._
+import org.apache.hadoop.conf._
+import org.apache.spark._
+import org.apache.spark.rdd._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.storage._
+import org.apache.spark.streaming._
+import scala.collection.JavaConversions._
+import scala.concurrent._
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+import spray.can.Http
+import spray.json._
+import DefaultJsonProtocol._
+import org.scalatest._
+import scala.annotation._
+import scala.math.Ordered.orderingToOrdered
+import scala.math.Ordering.Implicits._
+import scala.util.hashing._
+import java.text.SimpleDateFormat
+import java.util.Date
+import org.apache.spark.mllib.classification.{SVMModel,SVMWithSGD}
+import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
+import org.apache.spark.mllib.util.MLUtils
+import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.types.{StructType,StructField,StringType};
+
+import org.apache.spark.SparkException
+import org.apache.spark.sql.catalyst.plans.logical.{OneRowRelation, Union}
+import org.apache.spark.sql.execution.QueryExecution
+import org.apache.spark.sql.execution.aggregate.HashAggregateExec
+import org.apache.spark.sql.execution.exchange.{BroadcastExchangeExec, ReusedExchangeExec, ShuffleExchange}
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.internal.SQLConf
+import org.apache.spark.sql.types._
+
+import akka.testkit.TestKitBase
+
+class ProcessGen_Trans extends FlatSpec with Matchers with BeforeAndAfterAll with TestKitBase {
+
+    implicit lazy val system = ActorSystem()
+    implicit val timeout: Timeout = 1.minute
+
+    var fs: org.apache.hadoop.fs.FileSystem = null
+    var fs_conf: Configuration = null
+    var spark:SparkSession = null
+    var sc: org.apache.spark.SparkContext = null
+    var sqlContext: org.apache.spark.sql.SQLContext = null
+    
+    override def beforeAll() {
+        import org.apache.log4j.Logger
+        import org.apache.log4j.Level
+        Logger.getLogger("org").setLevel(Level.WARN)
+        Logger.getLogger("akka").setLevel(Level.WARN)
+        Logger.getLogger("parquet.hadoop").setLevel(Level.WARN)
+
+        fs_conf = new org.apache.hadoop.conf.Configuration
+        fs = FileSystem.get(fs_conf)
+
+        val conf = new org.apache.spark.SparkConf
+        conf.set("spark.master", "local[*]")
+        //conf.set("spark.master", "spark://192.168.20.17:7070")
+        conf.set("spark.app.name", "Aliyun")
+        conf.set("spark.ui.port", "55555")
+        conf.set("spark.default.parallelism", "10")
+        conf.set("spark.sql.shuffle.partitions", "10")
+        conf.set("spark.sql.shuffle.partitions", "1")
+        conf.set("spark.sql.autoBroadcastJoinThreshold", "1")
+        
+        spark = SparkSession.builder
+          .config(conf)
+          .getOrCreate()
+
+        sc = spark.sparkContext
+        sqlContext = spark.sqlContext
+        
+    }
+
+    override def afterAll() {
+        sc.stop()
+        fs.close()
+        system.shutdown()
+    }
+    
+    def reflectTest() = {
+      println("--------reflect----------")
+      "reflect success"
+    }
+
+    it should "run it" in {
+        val cfg = new Config("conf/server.properties")
+        
+        val jdbc = null
+        val ml = MLSample(sc,sqlContext)
+        val ss = SStream(sc)
+        val env = Env(system, cfg, fs, jdbc, ml, sc, sqlContext)
+        
+        val trans = Transform(spark)
+        val mlgen = MLGenetor(spark)
+        
+
+        try{
+            import scala.util._
+            import env.sqlContext.implicits._
+            
+            import scala.reflect.runtime.{universe => ru}
+            val typeMirror = ru.runtimeMirror(trans.getClass.getClassLoader)
+            val instanceMirror = typeMirror.reflect(trans)
+            val members = instanceMirror.symbol.typeSignature.members
+            val transMems = members.filter(_.name.decoded.startsWith("trans"))
+            val transNames = transMems.map(_.name.decoded).toSeq.sortBy(x=>x)
+            println(transNames)
+            
+            val zip = spark.read
+                //.schema(schema1)
+                .option("header", "true")
+                .option("inferSchema", "true")
+                .csv("data2/superzip.csv")
+            val superzipDS = zip.as("superzip")
+            
+            var transDS = superzipDS//.filter(x => {x.getInt(0) > 1100}).sort(features(0).name)
+            
+            transDS.printSchema()
+            transDS.show()
+            
+            val features = superzipDS.schema.fields
+            
+            val length = features.length * transNames.length
+            val sequence = Array.fill(length)(0).map(x => x ^ Random.nextInt(2))
+            
+            var handler = Seq[Tuple2[String, Seq[Int]]]()
+            
+            for(x <- transNames.zipWithIndex){
+              var transIdx = Seq[Int]()
+              for(y <- 0 to features.length-1){
+                if(sequence(x._2*10 + y) > 0){
+                  transIdx :+= y
+                }
+              }
+              handler :+= (x._1, transIdx)
+            }
+            
+            handler.foreach(x => {
+              val methodx = ru.typeOf[Transform].declaration(ru.newTermName(x._1)).asMethod
+              transDS = instanceMirror.reflectMethod(methodx)(transDS, x._2).asInstanceOf[Dataset[Row]]
+            })
+            
+            transDS.printSchema()
+            transDS.show()
+
+            
+        }
+        catch {
+            case t: Throwable =>
+                t.printStackTrace()
+        }
+    }
+
+
+}
